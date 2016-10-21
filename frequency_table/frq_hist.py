f7018c21a97632840b01b078694fff26fdf66b0b
liorH <liorhagigi@gmail.com>
2016-10-21 17:44:15 +0300

    numeric histograom in list

--- frequency_table/frq_hist.py
+++ frequency_table/frq_hist.py
@@ -8,9 +8,17 @@ my_string = f.read()
 str_spl = my_string.split(",")
 b = list(filter(lambda x: x.isnumeric(),str_spl))
 
+# init histogram  - create empty list for histogram
+hist = []
+for i in range(60):
+    hist.append(0)
 
 
+for x in b:
+    hist[int(x)] += 1
+ 
+      
 
-
+print(hist)
 print('the end')
 
