b6e6ecdb32c8c0045d4276a433bad1771506ddd5
liorH <liorhagigi@gmail.com>
2016-10-21 17:29:22 +0300

    read integers from list

--- frequency_table/frq_hist.py
+++ frequency_table/frq_hist.py
@@ -1,3 +1,16 @@
-print("hello world")
-a =10
-print(a)
+
+f = open('Lotto.csv','r')
+
+print ('the file Lotto read - ')
+my_string = f.read()
+
+
+str_spl = my_string.split(",")
+b = list(filter(lambda x: x.isnumeric(),str_spl))
+
+
+
+
+
+print('the end')
+
