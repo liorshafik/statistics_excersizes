32b6bc9d9ccbf762b056a44f68e21539c5410a71
liorH <liorhagigi@gmail.com>
2016-10-21 17:48:31 +0300

    ?

--- frequency_table/frq_hist.py
+++ frequency_table/frq_hist.py
@@ -8,6 +8,7 @@ my_string = f.read()
 str_spl = my_string.split(",")
 b = list(filter(lambda x: x.isnumeric(),str_spl))
 
+# init histogram  - create empty list for histogram:
 hist = []
 for i in range(60):
     hist.append(0)
