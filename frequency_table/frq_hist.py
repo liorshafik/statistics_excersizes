6b434a92142b9849efa2841c5c3a49631feaa7a2
liorH <liorhagigi@gmail.com>
2016-10-21 17:46:59 +0300

    add comments

--- frequency_table/frq_hist.py
+++ frequency_table/frq_hist.py
@@ -4,16 +4,15 @@ f = open('Lotto.csv','r')
 print ('the file Lotto read - ')
 my_string = f.read()
 
-
+# clean: 
 str_spl = my_string.split(",")
 b = list(filter(lambda x: x.isnumeric(),str_spl))
 
-# init histogram  - create empty list for histogram
 hist = []
 for i in range(60):
     hist.append(0)
 
-
+#count and add to histogram:
 for x in b:
     hist[int(x)] += 1
  
